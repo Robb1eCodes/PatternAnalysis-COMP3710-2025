5e08ac435ca80a5000b8e2a83e7bcdc261a06f77
Francis Crouch <crouch.f.r@gmail.com>
2025-11-01 16:15:16 +1000

    Update modules.py with Decoder + a summary when the file is ran.
    - Decoder adds 2D Convolution Transpose layers with concatenation for the skip connections.
    - Model is created to variable m, and summary printed when the file is ran.
    - Code for the activation layers was slightly wrong when trying to run the summary. Has been fixed.

--- recognition/HipMRI_Segmentation_48819099/modules.py
+++ recognition/HipMRI_Segmentation_48819099/modules.py
@@ -16,10 +16,10 @@ def double_conv(tensor, filters, conv_size = 3, activation = 'relu'):
 
     tensor = tf.keras.layers.Conv2D(filters, conv_size, padding='same')(tensor)
     tensor = tf.keras.layers.BatchNormalization()(tensor)
-    tensor = tf.keras.Activation(activation)(tensor)
+    tensor = tf.keras.layers.Activation(activation)(tensor)
     tensor = tf.keras.layers.Conv2D(filters, conv_size, padding='same')(tensor)
     tensor = tf.keras.layers.BatchNormalization()(tensor)
-    tensor = tf.keras.Activation(activation)(tensor)
+    tensor = tf.keras.layers.Activation(activation)(tensor)
 
     return tensor
 
@@ -58,3 +58,41 @@ def unet_2d(input_size = (256,256, 1), num_filters=32):
 
     # Bottleneck (End of Encoder)
     c5 = double_conv(p4, num_filters * 16)
+
+    # Decoder (upsampling)
+    # Up Block 4
+    u4 = tf.keras.layers.Conv2DTranspose(num_filters * 8, (2, 2), strides=(2, 2), padding='same')(c5)  # Upsample
+    u4 = tf.keras.layers.concatenate([u4, c4])  # Skip connection
+    c6 = double_conv(u4, num_filters * 8)
+
+    # Up Block 3
+    u3 = tf.keras.layers.Conv2DTranspose(num_filters * 4, (2, 2), strides=(2, 2), padding='same')(c6)
+    u3 = tf.keras.layers.concatenate([u3, c3])
+    c7 = double_conv(u3, num_filters * 4)
+
+    # Up Block 2
+    u2 = tf.keras.layers.Conv2DTranspose(num_filters * 2, (2, 2), strides=(2, 2), padding='same')(c7)
+    u2 = tf.keras.layers.concatenate([u2, c2])
+    c8 = double_conv(u2, num_filters * 2)
+
+    # Up Block 1
+    u1 = tf.keras.layers.Conv2DTranspose(num_filters, (2, 2), strides=(2, 2), padding='same')(c8)
+    u1 = tf.keras.layers.concatenate([u1, c1])
+    c9 = double_conv(u1, num_filters)
+
+    # Output layer: produces segmentation mask (1 channel, sigmoid activation)
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    # Build model
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+    return model
+
+if __name__ == '__main__':
+    """
+    Creates the model object, does not run any data through. 
+
+    Provides a summary of the model to check everything has been setup correctly.
+    """
+
+    m = unet_2d((128, 128, 1))
+    m.summary()
